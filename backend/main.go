c059801e33739a49402072909da446e9ebceab35
Adrydevmateo <adrydevmateo@gmail.com>
2026-09-17 23:47:10 -0400

    TODO added

--- backend/main.go
+++ backend/main.go
@@ -3,7 +3,6 @@ package main
 // TODO: Add JWT or OAuth or both
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 
@@ -18,34 +17,48 @@ type Project struct {
 	LiveURL string
 }
 
+func welcomeHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: return more relevant information
+	w.Write([]byte("Welcome to my website's REST API!"))
+}
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: create a good health endpoint
+	w.Write([]byte("Health"))
+}
+
+func projectsHandler(w http.ResponseWriter, r *http.Request) {
+	// TODO: get projects from a list
+	project := Project{Name: "Website", Banner: "Banner", LiveURL: "Live URL"}
+	j, err := json.Marshal(project)
+	if err != nil {
+		w.Write([]byte("Error getting json"))
+		return
+	}
+	w.Write(j)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(404)
+	w.Write([]byte("route does not exist"))
+}
+
+func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(405)
+	w.Write([]byte("method is not valid"))
+}
+
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.ClientIPFromHeader("CF-Connecting-IP"))
-	r.Use(httprate.LimitBy(1, time.Minute, func(r *http.Request) (string, error) {
+	r.Use(httprate.LimitBy(10, time.Minute, func(r *http.Request) (string, error) {
 		return httprate.CanonicalizeIP(middleware.GetClientIP(r.Context())), nil
 	}))
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
-	})
-	r.Get("/projects", func(w http.ResponseWriter, r *http.Request) {
-		project := Project{Name: "Website", Banner: "Banner", LiveURL: "Live URL"}
-		j, err := json.Marshal(project)
-		if err != nil {
-			w.Write([]byte("Error getting json"))
-			return
-		}
-		fmt.Println("Got JSON")
-		w.Write(j)
-	})
-	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(404)
-		w.Write([]byte("route does not exist"))
-	})
-	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(405)
-		w.Write([]byte("method is not valid"))
-	})
+	r.Get("/", welcomeHandler)
+	r.Get("/health", healthHandler)
+	r.Get("/projects", projectsHandler)
+	r.NotFound(notFoundHandler)
+	r.MethodNotAllowed(methodNotAllowedHandler)
 	http.ListenAndServe(":3000", r)
 }
