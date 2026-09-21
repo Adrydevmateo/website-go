1d3899dc3c4030fe7e8bebbc71ff5cda108ff108
Adrydevmateo <adrydevmateo@gmail.com>
2026-09-21 19:01:42 -0400

    pushing

--- backend/go.mod
+++ backend/go.mod
@@ -3,9 +3,22 @@ module website.com/backend
 go 1.27.1
 
 require (
-	github.com/go-chi/chi/v5 v5.3.2 // indirect
-	github.com/go-chi/httprate v0.16.0 // indirect
+	github.com/go-chi/chi/v5 v5.3.2
+	github.com/go-chi/httprate v0.16.0
+	github.com/go-chi/jwtauth/v5 v5.4.0
+)
+
+require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.3 // indirect
+	github.com/lestrrat-go/httpcc v1.0.1 // indirect
+	github.com/lestrrat-go/httprc/v3 v3.0.0-beta2 // indirect
+	github.com/lestrrat-go/jwx/v3 v3.0.2 // indirect
+	github.com/lestrrat-go/option v1.0.1 // indirect
+	github.com/segmentio/asm v1.2.0 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/crypto v0.56.0 // indirect
+	golang.org/x/sys v0.47.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,21 +1,73 @@
 package main
 
-// TODO: Add JWT or OAuth or both
+// TODO: Add JWT
 // TODO: return data encrypted
+// TODO: Create sign up and sign in endpoints
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/httprate"
+	"github.com/go-chi/jwtauth/v5"
 )
 
 type Project struct {
 	Name    string
 	Banner  string
 	LiveURL string
+}
+
+type User struct {
+	Fullname string
+	Email    string
+	Age      int
+}
+
+var tokenAuth *jwtauth.JWTAuth
+
+func init() {
+	tokenAuth = jwtauth.New("HS256", []byte("secret"), nil)
+}
+
+func main() {
+	http.ListenAndServe(":3000", router())
+}
+
+func router() http.Handler {
+	r := chi.NewRouter()
+
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.ClientIPFromHeader("CF-Connecting-IP"))
+	r.Use(httprate.LimitBy(10, time.Minute, rateLimitMiddlewareHandler))
+
+	r.Group(func(r chi.Router) {
+		r.Use(jwtauth.Verifier(tokenAuth))
+		r.Use(jwtauth.Authenticator(tokenAuth))
+
+		r.Get("/projects", projectsHandler)
+	})
+
+	r.Group(func(r chi.Router) {
+		r.Get("/", welcomeHandler)
+		r.Get("/health", healthHandler)
+
+		r.Post("/signin", signInHandler)
+		r.Post("/signup", signUpHandler)
+	})
+
+	r.NotFound(notFoundHandler)
+	r.MethodNotAllowed(methodNotAllowedHandler)
+
+	return r
+}
+
+func rateLimitMiddlewareHandler(r *http.Request) (string, error) {
+	return httprate.CanonicalizeIP(middleware.GetClientIP(r.Context())), nil
 }
 
 func welcomeHandler(w http.ResponseWriter, r *http.Request) {
@@ -27,8 +79,25 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Health"))
 }
 
+func signInHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	w.Write([]byte("Sign In"))
+}
+
+func signUpHandler(w http.ResponseWriter, r *http.Request) {
+	user := User{Fullname: "Adry Mateo Ramon", Email: "adry@gmail.com"}
+	_, tokenString, err := tokenAuth.Encode(map[string]any{"name": user.Fullname, "email": user.Email})
+	if err != nil {
+		w.Write([]byte("error generating the jwt"))
+		return
+	}
+	w.Write([]byte(tokenString))
+}
+
 func projectsHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO: get projects from a list
+	_, claims, _ := jwtauth.FromContext(r.Context())
+	fmt.Fprintf(w, "protected area. hi %v", claims["name"])
 	project := Project{Name: "Website", Banner: "Banner", LiveURL: "Live URL"}
 	j, err := json.Marshal(project)
 	if err != nil {
@@ -46,20 +115,4 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 func methodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(405)
 	w.Write([]byte("method is not valid"))
-}
-
-func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.ClientIPFromHeader("CF-Connecting-IP"))
-	r.Use(httprate.LimitBy(10, time.Minute, func(r *http.Request) (string, error) {
-		return httprate.CanonicalizeIP(middleware.GetClientIP(r.Context())), nil
-	}))
-	r.Get("/", welcomeHandler)
-	r.Get("/health", healthHandler)
-	r.Get("/projects", projectsHandler)
-	r.NotFound(notFoundHandler)
-	r.MethodNotAllowed(methodNotAllowedHandler)
-	http.ListenAndServe(":3000", r)
 }
